932da718af34805071e42c5a3adadec5d2cfc9bb
Miguel Ángel Rodríguez <miguel-angel.rodriguez@prodigious.com>
2021-03-19 18:53:59 -0500

    Got to create initial geolocation ip project

---
+++ README.md
@@ -0,0 +1,13 @@
+# **Appgate - Geolocation IP**
+
+### **Run Local**
+Execute the next command on command line console
+```
+    ./run-local
+```
+
+### **Run Docker**
+Execute the next command on command line console
+```
+    ./run-docker
+```
---
+++ src/test/java/com/appgate/geolocationip/GeolocationIpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appgate.geolocationip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeolocationIpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
